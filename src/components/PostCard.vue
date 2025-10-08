f5f765d01d7d409334039fccdfd87bbad1063125
Nico <nbalvarezo@gmail.com>
2025-10-07 21:04:53 -0300

    Se creo el componente PostCard

---
+++ src/components/PostCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="post-card">
+        <h3>Titulo</h3>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda tempora saepe culpa maxime, doloremque
+            labore nulla dolorem voluptatem deserunt temporibus nemo iure, ut optio adipisci inventore odit sit eum
+            omnis.
+        </p>
+        <PostButton/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+    import PostButton from './PostButton.vue';
+
+
+</script>
+
+<style scoped>
+    .post-card {
+        border-radius: 15px;
+        padding: 10px;
+        margin: 5px;
+        border: 1px solid black;
+    }
+</style>
