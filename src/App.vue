681f672a53273b4287f73e7cf4e4b96f0b4d1a5d
Nico <nbalvarezo@gmail.com>
2025-10-08 14:40:57 -0300

    Se muestra el componente CounterOption y se comenta PostContainer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <h1>Listado de posts: </h1>
-  <PostContainer />
+  <!-- <h1>Listado de posts: </h1>
+  <PostContainer /> -->
+  
+  <CounterOption />
 </template>
 
 <script lang="ts" setup>
 
-import PostContainer from './components/PostContainer.vue';
-
+// import PostContainer from './components/PostContainer.vue';
+import CounterOption from './components/CounterOption.vue';
 </script>
 
 <style>
